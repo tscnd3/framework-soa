7b94ef3fb15253fa2bf664385c9e9693593f5db8
xiangtao <xiangtao@zywamail.com>
2020-06-28 11:00:21 +0800

    fix:修该disconf

--- framework-disconf/src/main/java/com/xinyue/framework/disconf/config/constant/DisconfConstant.java
+++ framework-disconf/src/main/java/com/xinyue/framework/disconf/config/constant/DisconfConstant.java
@@ -1,20 +1,8 @@
-/**   
- * Copyright © 2019 qmdy. Tech Ltd. All rights reserved.
- * 
- * 功能描述：
- * @Package: cn.idianyou.framework.config 
- * @author: Zhihao Peng   
- * @date: 2019年5月8日 上午1:48:28 
- */
 package com.xinyue.framework.disconf.config.constant;
 
-/**    
-* @ClassName: DisconfConstant.java
-* @Description: 常量
-* @version: v1.0.0
-*/
-public class DisconfConstant {
-	public static final String DEFAULT_CORE_SCAN_PACKAGE = "com.xinyue.business";
+public interface DisconfConstant {
+	
+	public static final String DEFAULT_CORE_SCAN_PACKAGE = "com.xinyue.framework.disconf.config.core";
 	
 	public static final String APPLICATION_PROPERTIES = "application.properties";
 }
